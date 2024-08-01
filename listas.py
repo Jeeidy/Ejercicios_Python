556754763045cbd72b2711ecdb079f288cb21948
Jeeidy <eytrujillo24@soy.sena.edu.co>
2024-07-31 22:10:04 -0500

    listas

---
+++ listas.py
@@ -0,0 +1,33 @@
+#Obtener un elemento
+
+'''shopping = ['Agua', 'Huevos', 'Aceite']
+
+print(shopping[0])'''
+
+#Añadir a la final de la lista
+
+'''shopping = ['Agua', 'Huevos', 'Aceite']
+
+shopping.append('Atún')
+
+print(shopping)'''
+
+#Creando desde vacío 
+
+'''even_numbers = []
+
+for i in range(20):
+    if i % 2 == 0:
+        even_numbers.append(i)
+    print(even_numbers)'''
+
+#Añadir en cualquier posición de una lista
+
+'''shopping = ['Agua', 'Huevos', 'Aceite']
+
+shopping.insert(1, 'Jamón')
+
+shopping
+['Agua', 'Jamón', 'Huevos', 'Aceite']
+
+shopping.insert(3, 'Queso')'''
